df68847cfaacb086351a69043d0cd7d2f288bb7b
Soomin Kimu <soominkimu@gmail.com>
2019-04-03 21:25:01 +0900

    sq3d-polygon, planet-earth

---
+++ planet-earth/precache-manifest.72d9d2eca205f2aa3d6483374e7cd955.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72620d75849fa46487de",
+    "url": "/planet-earth/static/css/main.ad240412.chunk.css"
+  },
+  {
+    "revision": "72620d75849fa46487de",
+    "url": "/planet-earth/static/js/main.72620d75.chunk.js"
+  },
+  {
+    "revision": "bea3587c8e9823f8ed52",
+    "url": "/planet-earth/static/js/1.bea3587c.chunk.js"
+  },
+  {
+    "revision": "28490b51427ed02f3c56",
+    "url": "/planet-earth/static/js/runtime~main.28490b51.js"
+  },
+  {
+    "revision": "e2c85236aab3f3880b269029041d0924",
+    "url": "/planet-earth/index.html"
+  }
+];
---
+++ sq3d-polygon/precache-manifest.03ccb4aa099a27c270d28a9678dc37ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af18abdb3e0d1e7965c7",
+    "url": "/sq3d-polygons/static/css/main.e50146b9.chunk.css"
+  },
+  {
+    "revision": "af18abdb3e0d1e7965c7",
+    "url": "/sq3d-polygons/static/js/main.0f4c8262.chunk.js"
+  },
+  {
+    "revision": "8fef9a3cbcfacb6269b7",
+    "url": "/sq3d-polygons/static/js/runtime~main.88198f10.js"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/sq3d-polygons/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "c8409e5ef15de142520b2c023dbf23d6",
+    "url": "/sq3d-polygons/index.html"
+  }
+];
