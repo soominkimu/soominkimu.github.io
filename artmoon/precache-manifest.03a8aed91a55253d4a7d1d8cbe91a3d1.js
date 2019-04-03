3dce11038fbab30178459ff867fe7ac8af83a881
Soomin Kimu <soominkimu@gmail.com>
2019-04-03 20:37:00 +0900

    artmoon

---
+++ artmoon/precache-manifest.03a8aed91a55253d4a7d1d8cbe91a3d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d44510f44d95359b188b",
+    "url": "/artmoon/static/css/main.cfcb42be.chunk.css"
+  },
+  {
+    "revision": "d44510f44d95359b188b",
+    "url": "/artmoon/static/js/main.d44510f4.chunk.js"
+  },
+  {
+    "revision": "b95f6fcd338a4870ef47",
+    "url": "/artmoon/static/js/1.b95f6fcd.chunk.js"
+  },
+  {
+    "revision": "40a700df3c2000df1582",
+    "url": "/artmoon/static/js/runtime~main.40a700df.js"
+  },
+  {
+    "revision": "96fd6397fde2fd0323f0356363d02be0",
+    "url": "/artmoon/static/media/cloud02.96fd6397.png"
+  },
+  {
+    "revision": "866a3f83d30e42c68d6dcaa14ea3bddf",
+    "url": "/artmoon/index.html"
+  }
+];
---
+++ artmoon/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/artmoon/precache-manifest.03a8aed91a55253d4a7d1d8cbe91a3d1.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/artmoon/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
