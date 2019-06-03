20176fb90f199e39cd34448f6bcd3ef997da8b4c
Soomin Kimu <soominkimu@gmail.com>
2019-06-03 18:38:02 +0900

    Changed to spacetimeq logo project

---
+++ precache-manifest.7b03e48a34f1f9db5f46c795a83e4a34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40bd970611ffa054fe8800f14499a76c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "447f372752f4660aa931",
+    "url": "/static/css/main.e2913b77.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "447f372752f4660aa931",
+    "url": "/static/js/main.861b0bb6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
